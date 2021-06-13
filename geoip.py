5f3a848ea3d0525e0d6a63456ad82ad823827825
Sonjoaquin <39743051+Sonjoaquin@users.noreply.github.com>
2021-06-13 23:50:37 +0200

    geoip python script
    Added geoiplookup python script

---
+++ geoip.py
@@ -0,0 +1,17 @@
+import subprocess
+import sys
+from multiprocessing import Pool
+
+pool = Pool()
+posicion = 1
+
+comando = "%s" % (sys.argv[posicion])
+if comando.endswith('.txt'):
+    with open(comando) as f:
+        for line in f:
+            pool.apply_async(subprocess.call(["geoiplookup",line.strip()]))
+else:
+    parametros = len(sys.argv) - 1
+    while (parametros >= posicion):
+        pool.apply_async(subprocess.call(["geoiplookup", comando]))
+        posicion = posicion + 1
